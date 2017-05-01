df894c6b8b6947e46765e6340047d12e8b3ad3f7
nasum <masaya.nasu@crowdworks.co.jp>
2017-05-01 18:26:26 +0900

    first commit

---
+++ sandbox.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  fmt.Println("Wellcome to Playground!")
+
+  fmt.Println("The time is", time.Now())
+}
---
+++ type-inference.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+  v := "hoge"
+  fmt.Printf("v is type %T\n", v)
+}

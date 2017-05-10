e88de8c9702afa50f0aacdb62567acf12401c26d
nasum <masaya.nasu@crowdworks.co.jp>
2017-05-10 14:44:22 +0900

    append map range

---
+++ exercise-slices.go
@@ -0,0 +1,22 @@
+package main
+
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+	a := make([][]uint8, dy)
+	for i := range a {
+		a[i] = make([]uint8, dx)
+	}
+
+	for i := range a {
+		for j := range a[i] {
+			a[i][j] = uint8((i + j) / 2)
+		}
+	}
+
+	return a
+}
+
+func main() {
+	pic.Show(Pic)
+}
